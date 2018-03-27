0e025f2a87bffc8382558439fdddae13e82cd386
Your Name <you@example.com>
2018-03-27 17:05:55 -0400

    Commit for quiz3

---
+++ exercise/wk3/dbPractice1.c
@@ -0,0 +1,30 @@
+//factorial of a number is the product of the number with all
+// the numbers below it.
+//eg: Factorial of 5 = 5 * 4 * 3 * 2 * 1 = 120
+
+
+int factorial{int);
+
+int main(void)
+{
+        using namespace std;
+    printf( "Debugging Practice 1 - Quiz 3, Q3\n\n");
+	    #include<stdio.h>
+        printf( "Please enter the number whose factorial you wish to find: ");
+        scanf("%d", &n);
+
+        int fact = factorial(n);
+        printf("The factorial of %f is %d", &n,factorial)
+
+    return 0;
+}
+}
+
+int factorial(int number)
+{
+    int factorial = 1;
+    for (int i =1; i<=num; i--)
+            factorial =* i;
+
+    return fact;
+}
